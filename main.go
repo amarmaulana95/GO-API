c8c89b823b429a7d8f31f3535d88603f7a48fde7
amarmaulana95 <amar.unpak@gmail.com>
2020-10-16 16:45:25 +0700

    #3
    update repository

--- main.go
+++ main.go
@@ -2,29 +2,17 @@ package main
 
 import (
 	"log"
-	"net/http"
-	"os/user"
 
-	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func main() {
-	router := gin.Default()
-	router.GET("/handler", handler)
-	router.Run()
-}
-
-func handler(c *gin.Context) {
 	dsn := "root@tcp(127.0.0.1:3306)/db_go?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		log.Fatal(err.Error())
 	}
-	var users []user.User
-	db.Find(&users)
 
-	c.JSON(http.StatusOK, users)
 }
