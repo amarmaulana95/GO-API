741480458e9a880ea6ca890513650fe7b35ea9ee
amarmaulana95 <amar.unpak@gmail.com>
2020-10-16 14:53:17 +0700

    #2
     update module user

--- main.go
+++ main.go
@@ -1,32 +1,30 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"net/http"
 	"os/user"
 
+	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func main() {
+	router := gin.Default()
+	router.GET("/handler", handler)
+	router.Run()
+}
+
+func handler(c *gin.Context) {
 	dsn := "root@tcp(127.0.0.1:3306)/db_go?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		log.Fatal(err.Error())
 	}
-	fmt.Println("koneksi ok")
-
-	// tampilkan user, masukan ke array of
 	var users []user.User
-	length := len(users)
-
-	fmt.Println(length)
-
 	db.Find(&users)
 
-	length = len(users)
-	fmt.Println(length)
-
+	c.JSON(http.StatusOK, users)
 }
