c7ede95374acc3830da809c881873fd59a18748f
amarmaulana95 <amar.unpak@gmail.com>
2020-10-21 23:34:00 +0700

    #18 update format json campaign

--- campaign/formater.go
+++ campaign/formater.go
@@ -1,1 +1,40 @@
 package campaign
+
+type CampaignFormatter struct {
+	ID               int    `json:"id"`
+	userID           int    `json:"user_id"`
+	Name             string `json:"name"`
+	ShortDescription string `json:"short_description"`
+	ImageUrl         string `json:"image_url"`
+	GoalAmount       int    `json:"goal_amount"`
+	CurrentAmount    int    `json:"current_amount"`
+}
+
+func FormatCampaign(campaign Campaign) CampaignFormatter {
+	//buat object
+	CampaignFormatter := CampaignFormatter{}
+	CampaignFormatter.ID = campaign.ID
+	CampaignFormatter.userID = campaign.userID
+	CampaignFormatter.Name = campaign.Name
+	CampaignFormatter.ShortDescription = campaign.ShortDescription
+	CampaignFormatter.GoalAmount = campaign.GoalAmount
+	CampaignFormatter.CurrentAmount = campaign.CurrentAmount
+	CampaignFormatter.ImageUrl = ""
+
+	if len(campaign.CampaignImages) > 0 {
+		CampaignFormatter.ImageUrl = campaign.CampaignImages[0].FileName
+	}
+	return CampaignFormatter
+}
+
+func FormatCampaigns(campaigns []Campaign) []CampaignFormatter {
+	//array campaign formater
+
+	var campaignsFormatter []CampaignFormatter
+
+	for _, campaign := range campaigns {
+		campaignFormatter := FormatCampaign(campaign)
+		campaignsFormatter = append(campaignsFormatter, campaignFormatter)
+	}
+	return campaignsFormatter
+}
--- handler/campaign.go
+++ handler/campaign.go
@@ -29,7 +29,7 @@ func (h *campaignHandler) GetCampaigns(c *gin.Context) {
 
 	campaigns, err := h.service.GetCampaigns(userID)
 	if err != nil {
-		response := helper.APIResponse("Error get data campaigns ", http.StatusBadRequest, "error", nil)
+		response := helper.APIResponse("Error get data campaigns ", http.StatusBadRequest, "error", campaign.FormatCampaigns(campaigns))
 		c.JSON(http.StatusBadRequest, response)
 		return
 	}
