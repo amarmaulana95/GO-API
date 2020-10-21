0cdd80cdbf49d58cfc8babe179c4a9d04258bf22
amarmaulana95 <amar.unpak@gmail.com>
2020-10-21 22:50:27 +0700

    #17 list data campaigns

--- handler/campaign.go
+++ handler/campaign.go
@@ -1,7 +1,39 @@
 package handler
 
+import (
+	"gomar/campaign"
+	"gomar/helper"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
 // tangkap param di handler
 // handler ke service
 // service menentukan repository mana yg akan di call
 // repo : GetAll GetUserbyID
 // db
+
+type campaignHandler struct {
+	service campaign.Service
+}
+
+func NewCampaignHandler(service campaign.Service) *campaignHandler {
+	return &campaignHandler{service}
+}
+
+//api/v1/campaigns
+func (h *campaignHandler) GetCampaigns(c *gin.Context) {
+	userID, _ := strconv.Atoi(c.Query("user_id")) //ubah ke int dngn strconv.atoi
+
+	campaigns, err := h.service.GetCampaigns(userID)
+	if err != nil {
+		response := helper.APIResponse("Error get data campaigns ", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+	response := helper.APIResponse("List data campaigns ", http.StatusOK, "success", campaigns)
+	c.JSON(http.StatusBadRequest, response)
+	return
+}
--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"gomar/auth"
 	"gomar/campaign"
 	"gomar/handler"
@@ -28,13 +27,12 @@ func main() {
 
 	campaignRepository := campaign.NewRepository(db)
 	campaignService := campaign.NewService(campaignRepository)
-	campaigns, _ := campaignService.FindCampaigns(9)
-	fmt.Println(len(campaigns))
 
 	userService := user.NewService(userRepository)
 	authService := auth.NewService()
 
 	userHandler := handler.NewUserHandler(userService, authService)
+	campaignHandler := handler.NewCampaignHandler(campaignService)
 
 	router := gin.Default()
 	api := router.Group("api/v1")
@@ -43,6 +41,9 @@ func main() {
 	api.POST("/sessions", userHandler.Login)
 	api.POST("/email_checkers", userHandler.CekEmailDB)
 	api.POST("/avatars", authMiddleware(authService, userService), userHandler.UploadAvatar)
+
+	api.GET("/campaigns", campaignHandler.GetCampaigns)
+
 	router.Run()
 
 	// input dari user
