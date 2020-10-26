94e7cf5978be2aa83db20cc091527bc392132afa
amarmaulana95 <amar.unpak@gmail.com>
2020-10-26 22:46:55 +0700

    #19 get data campaigns endpoint formatter

--- handler/campaign.go
+++ handler/campaign.go
@@ -29,11 +29,11 @@ func (h *campaignHandler) GetCampaigns(c *gin.Context) {
 
 	campaigns, err := h.service.GetCampaigns(userID)
 	if err != nil {
-		response := helper.APIResponse("Error get data campaigns ", http.StatusBadRequest, "error", campaign.FormatCampaigns(campaigns))
+		response := helper.APIResponse("Error get data campaigns ", http.StatusBadRequest, "error", nil)
 		c.JSON(http.StatusBadRequest, response)
 		return
 	}
-	response := helper.APIResponse("List data campaigns ", http.StatusOK, "success", campaigns)
+	response := helper.APIResponse("List data campaigns ", http.StatusOK, "success", campaign.FormatCampaigns(campaigns))
 	c.JSON(http.StatusBadRequest, response)
 	return
 }
